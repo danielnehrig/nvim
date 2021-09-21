8b149ae97986053557a85993ae43eefbac6440ea
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-09-21 20:20:23 +0200

    check is nvim dir
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -292,6 +292,7 @@ local function init()
     use({ "ggandor/lightspeed.nvim", keys = { "s", "S", "t", "f", "T", "F" } }) -- lightspeed motion
 
     -- quality of life
+    use({ "wakatime/vim-wakatime" })
     use({
         "t9md/vim-choosewin",
         cmd = { "ChooseWin" },
--- lua/plugins/lspconfig/lua.lua
+++ lua/plugins/lspconfig/lua.lua
@@ -13,12 +13,15 @@ if not packer_plugins["lua-dev.nvim"].loaded then
     vim.cmd([[packadd lua-dev.nvim]])
 end
 
+local isNvim = string.match(vim.fn.getcwd(), ".*/nvim.*") ~= nil and true
+    or false
+
 local luadev = require("lua-dev").setup({
     library = {
-        vimruntime = true, -- runtime path
-        types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-        plugins = true, -- installed opt or start plugins in packpath
-        nvim_cfg = true,
+        vimruntime = isNvim, -- runtime path
+        types = isNvim, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+        plugins = isNvim, -- installed opt or start plugins in packpath
+        nvim_cfg = isNvim,
         -- you can also specify the list of plugins to make available as a workspace library
         -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
     },
@@ -35,7 +38,7 @@ local luadev = require("lua-dev").setup({
         flags = { debounce_text_changes = 500 },
         -- root_dir = require("lspconfig/util").root_pattern("."),
         on_attach = function(client, bufnr)
-            -- client.resolved_capabilities.document_formatting = false
+            client.resolved_capabilities.document_formatting = false
             lsp:on_attach(client, bufnr)
         end,
     },
