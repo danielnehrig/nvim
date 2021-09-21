49acf8826ab2fe1378129c26fa748b75e012a1ea
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-09-21 17:48:34 +0200

    fix(telescope): fzf
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -53,12 +53,17 @@ function M.init()
         },
         extensions = {
             fzf = {
+                fuzzy = true,
                 override_generic_sorter = true, -- override the generic sorter
                 override_file_sorter = true, -- override the file sorter
                 case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             },
             project = {
-                base_dirs = { "~/code" },
+                base_dirs = {
+                    "~/code",
+                    "~/dotfiles",
+                    "~/dotfiles/.config/nvim",
+                },
                 max_depth = 4,
                 hidden_files = true,
             },
