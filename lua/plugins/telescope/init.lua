0351fd166a699baf669363e26e76259cdc400d35
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-09-21 17:45:34 +0200

    fix(telescope): hidden files
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -2,13 +2,13 @@ local M = {}
 M.__index = M
 
 function M.init()
-    vim.cmd [[packadd plenary.nvim]]
-    vim.cmd [[packadd telescope-project.nvim]]
-    vim.cmd [[packadd telescope-fzf-native.nvim]]
-    vim.cmd [[packadd octo.nvim]]
+    vim.cmd([[packadd plenary.nvim]])
+    vim.cmd([[packadd telescope-project.nvim]])
+    vim.cmd([[packadd telescope-fzf-native.nvim]])
+    vim.cmd([[packadd octo.nvim]])
 
     local telescope = require("telescope")
-    telescope.setup {
+    telescope.setup({
         defaults = {
             vimgrep_arguments = {
                 "rg",
@@ -17,7 +17,7 @@ function M.init()
                 "--with-filename",
                 "--line-number",
                 "--column",
-                "--smart-case"
+                "--smart-case",
             },
             hidden = true,
             prompt_prefix = "🔍 ",
@@ -27,38 +27,48 @@ function M.init()
             selection_strategy = "reset",
             sorting_strategy = "descending",
             layout_strategy = "horizontal",
-            file_sorter = require "telescope.sorters".get_generic_sorter,
-            file_ignore_patterns = {".git", "node_modules"},
-            generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
+            file_sorter = require("telescope.sorters").get_generic_sorter,
+            file_ignore_patterns = { ".git", "node_modules" },
+            generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
             winblend = 0,
             border = {},
-            borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+            borderchars = {
+                "─",
+                "│",
+                "─",
+                "│",
+                "╭",
+                "╮",
+                "╯",
+                "╰",
+            },
             color_devicons = true,
             use_less = true,
-            set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
-            file_previewer = require "telescope.previewers".vim_buffer_cat.new,
-            grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
-            qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
+            set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
             -- Developer configurations: Not meant for general override
-            buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
+            buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         },
         extensions = {
             fzf = {
                 override_generic_sorter = true, -- override the generic sorter
                 override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             },
             project = {
-                base_dirs = {"~/code"},
-                max_depth = 4
-            }
-        }
-    }
+                base_dirs = { "~/code" },
+                max_depth = 4,
+                hidden_files = true,
+            },
+        },
+    })
     telescope.load_extension("fzf")
     telescope.load_extension("project")
     telescope.load_extension("dotfiles")
     telescope.load_extension("file_create")
-    require "octo".setup()
+    require("octo").setup()
 end
 
 return M
