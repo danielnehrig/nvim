c31e0c49f62c80c4370365ac44d43d26cbcd107e
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-09-27 22:53:01 +0200

    fix(jdtls): WOOO
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/lspconfig/java.lua
+++ lua/plugins/lspconfig/java.lua
@@ -4,9 +4,8 @@ local lspconfig = require("lspconfig")
 
 local home = require("core.global").home
 local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace = home .. "/code/work/" .. workspace_dir
+local workspace = home .. "/code/workspace/" .. workspace_dir
 
-vim.lsp.set_log_level("trace")
 lspconfig.jdtls.setup({
   filetypes = { "java" },
   cmd = { "jdtls", "-data", workspace },
