15384082c4f26817db90ebe1625b98fd772c54ab
Daniel Nehrig <daniel.nehrig@redteclab.com>
2021-09-29 11:36:48 +0200

    comment python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,4 @@
+-- TODO: trigger on python filetype when mapping is pressed then add dap-python
 require("plugins.dap.attach"):addPlug()
 vim.cmd([[ packadd nvim-dap-python ]])
 require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
