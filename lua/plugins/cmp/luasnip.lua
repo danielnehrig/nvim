9c65deedd993803495e1dacc6dda9e9f02c55862
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-09-29 02:58:00 +0200

    fix(plug): add choice indicator
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/cmp/luasnip.lua
+++ lua/plugins/cmp/luasnip.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 function M.init()
+  local types = require("luasnip.util.types")
   require("luasnip.config").setup({
+    ext_opts = {
+      [types.choiceNode] = {
+        active = {
+          virt_text = { { "●", "GruvboxOrange" } },
+        },
+      },
+      [types.insertNode] = {
+        active = {
+          virt_text = { { "●", "GruvboxBlue" } },
+        },
+      },
+    },
     history = true,
     updateevents = "TextChanged,TextChangedI",
     delete_check_events = "TextChanged,TextChangedI",
