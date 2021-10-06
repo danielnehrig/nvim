6215bc3a9258bf29cf8a072bc81f4abf7f6cffb1
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-10-07 00:07:38 +0200

    fix(lsp): cssls forgot capa
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/lspconfig/css.lua
+++ lua/plugins/lspconfig/css.lua
@@ -1,7 +1,9 @@
 local lspconfig = require("lspconfig")
 local lsp = require("plugins.lspconfig")
+local capabilities = require("plugins.lspconfig.capabilities").capabilities
 
 lspconfig.cssls.setup({
+  capabilities = capabilities,
   on_attach = function(client, bufnr)
     lsp.on_attach(client, bufnr)
   end,
