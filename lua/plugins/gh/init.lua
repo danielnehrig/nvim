41db6f5cfe1a1150f7be2ba53a1a3bbbb6ebd29e
Daniel Nehrig <daniel.nehrig@redteclab.com>
2021-10-11 08:06:59 +0200

    add check for env

--- lua/plugins/gh/init.lua
+++ lua/plugins/gh/init.lua
@@ -1,10 +1,17 @@
 local M = {}
 
 function M.init()
-  require("github-notifications").setup({
-    username = os.getenv("GH_REGISTRY_USERNAME"),
-    token = os.getenv("GH_TOKEN"),
-  })
+  local username = os.getenv("GH_REGISTRY_USERNAME")
+  local token = os.getenv("GH_TOKEN")
+  local username_len = string.len(username)
+  local token_len = string.len(token)
+
+  if username_len > 1 and token_len > 1 then
+    require("github-notifications").setup({
+      username = username,
+      token = token,
+    })
+  end
 end
 
 function M.load()
