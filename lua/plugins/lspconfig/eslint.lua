70ef8d2bb4707f0dd3470019f4a67c2e170d9998
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-10-16 02:28:20 +0200

    capa
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/lspconfig/eslint.lua
+++ lua/plugins/lspconfig/eslint.lua
@@ -1,7 +1,10 @@
 local lspconfig = require("lspconfig")
 local lsp = require("plugins.lspconfig")
+local capabilities = require("plugins.lspconfig.capabilities").capabilities
 
 lspconfig.eslint.setup({
+  capabilities = capabilities,
+  flags = { debounce_text_changes = 500 },
   on_attach = function(client, bufnr)
     lsp.on_attach(client, bufnr)
   end,
