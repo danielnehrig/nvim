4f7d7fd9889d14b4a549a89474256507117b54be
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-11-07 21:53:49 +0100

    orgmode ts
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,6 +1,15 @@
 local M = {}
 
 function M.init()
+  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  parser_config.org = {
+    install_info = {
+      url = "https://github.com/milisims/tree-sitter-org",
+      revision = "main",
+      files = { "src/parser.c", "src/scanner.cc" },
+    },
+    filetype = "org",
+  }
   require("nvim-treesitter.configs").setup({
     -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
