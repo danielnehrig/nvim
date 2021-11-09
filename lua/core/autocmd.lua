71d05338354edf3b8f1b8cc30fdec77ad74b5e60
Daniel Nehrig <daniel.nehrig@shop-apotheke.com>
2021-11-09 17:45:54 +0100

    autocmd bufline
    Signed-off-by: Daniel Nehrig <daniel.nehrig@shop-apotheke.com>

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -5,40 +5,61 @@ function M.autocmds()
   local definitions = {
     ft = {
       { "FileType", "NvimTree,lspsagafinder,dashboard", "let b:cusorword=0" },
+      {
+        "WinEnter,BufRead,BufEnter",
+        "dashboard",
+        "Dashboard",
+      }, -- disable tabline in dashboard
+      { "BufNewFile,BufRead", "*.toml", "setf toml" }, -- set toml filetype
+      {
+        "FileType",
+        "*.toml",
+        "lua require('cmp').setup.buffer { sources = { { name = 'crates' } } }",
+      },
+      {
+        "FileType",
+        "*.org",
+        "lua require('cmp').setup.buffer { sources = { { name = 'orgmode' } } }",
+      },
+    },
+    Terminal = {
+      { "TermOpen", "*", "set nonumber" },
+      { "TermOpen", "*", "set norelativenumber" },
+      { "TermOpen", "*", "set showtabline=0" }, -- renable it
+      { "WinEnter,BufEnter", "terminal", "set nonumber" },
+      { "WinEnter,BufEnter", "terminal", "set norelativenumber" },
+      { "WinEnter,BufEnter", "terminal", "set showtabline=0" }, -- renable it
+    },
+    TabLine = {
       { "FileType", "dashboard,TelescopePrompt,prompt", "set showtabline=0" }, -- disable tabline in dashboard
       {
         "WinLeave,WinClosed,BufLeave,BufDelete,BufUnload,BufWinLeave,BufWipeout",
         "TelescopePrompt,prompt",
         "set showtabline=2",
       }, -- disable tabline in dashboard
-      { "BufNewFile,BufRead,WinEnter", "*.*", "set showtabline=2" }, -- renable it
-      { "TermOpen", "*", "set nonumber" },
-      { "TermOpen", "*", "set norelativenumber" },
-      { "TermOpen", "*", "set showtabline=0" }, -- renable it
-      { "BufNewFile,BufRead", "*.toml", " setf toml" }, -- set toml filetype
       {
-        "FileType",
-        "*.toml",
-        "lua require('cmp').setup.buffer { sources = { { name = 'crates' } } }",
+        "BufNewFile,BufRead,WinEnter,TermLeave",
+        "*.*",
+        "set showtabline=2",
+      }, -- renable it
+    },
+    ScrollbarInit = {
+      {
+        "CursorMoved,VimResized,QuitPre",
+        "*",
+        "silent! lua require('scrollbar').show()",
+      },
+      {
+        "WinEnter,FocusGained",
+        "*",
+        "silent! lua require('scrollbar').show()",
+      },
+      {
+        "WinLeave,BufLeave,BufWinLeave,FocusLost",
+        "*",
+        "silent! lua require('scrollbar').clear()",
       },
     },
-    --  ScrollbarInit = {
-    --  {
-    --  "CursorMoved,VimResized,QuitPre",
-    --  "*",
-    --  "silent! lua require('scrollbar').show()",
-    --  },
-    --  {
-    --  "WinEnter,FocusGained",
-    --  "*",
-    --  "silent! lua require('scrollbar').show()",
-    --  },
-    --  {
-    --  "WinLeave,BufLeave,BufWinLeave,FocusLost",
-    --  "*",
-    --  "silent! lua require('scrollbar').clear()",
-    --  },
-    --  },
     --  gh = {
     --  { "DirChanged", "*", "silent! lua require('plugins.gh').load()" },
     --  },
