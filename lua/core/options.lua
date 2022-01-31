17dae458719073570b1ec730a5ff47092ab17dd9
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-01-31 19:23:08 +0100

    gui font fix
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -55,7 +55,7 @@ function M.load_options()
 
   opt.signcolumn = "auto:2" -- 2 sign column
   opt.cmdheight = 1 -- ex cmd height
-  opt.guifont = "Fira Code Regular Nerd Font:h12" -- set font
+  opt.guifont = "Fira Code Regular Nerd Font Complete:h12" -- set font
   opt.showcmd = false -- disable showcmd keys bottom right
   opt.showmode = false -- modes
   opt.autoread = true -- reload files changed other edit
