12e3dac3d11b4ae2bdb059449fc361c71c36016c
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-02-26 16:34:30 +0100

    pretty fold change
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/config/packer-config/init.lua
+++ lua/config/packer-config/init.lua
@@ -350,7 +350,28 @@ local function init()
   use({
     "anuvyklack/pretty-fold.nvim",
     config = function()
-      require("pretty-fold").setup({ remove_fold_markers = false })
+      require("pretty-fold").setup({
+        keep_indentation = false,
+        fill_char = "━",
+        sections = {
+          left = {
+            "━ ",
+            function()
+              return string.rep("*", vim.v.foldlevel)
+            end,
+            " ━┫",
+            "content",
+            "┣",
+          },
+          right = {
+            "┫ ",
+            "number_of_folded_lines",
+            ": ",
+            "percentage",
+            " ┣━━",
+          },
+        },
+      })
       require("pretty-fold.preview").setup({ border = "rounded" })
     end,
   })
