590b41d0b401d00947cba1e3b2884842dfd4fd67
Daniel Nehrig <daniel.nehrig@redteclab.com>
2022-03-14 11:53:03 +0100

    minimal update
    Signed-off-by: Daniel Nehrig <daniel.nehrig@redteclab.com>

--- minimal.lua
+++ minimal.lua
@@ -10,15 +10,100 @@ local function load_plugins()
       {
         "nvim-telescope/telescope.nvim",
         requires = {
-          "nvim-lua/plenary.nvim",
-          {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            run = "make",
-          },
+          "nvim-lua/plenary.nvim"
         },
       },
       {
         "nvim-treesitter/nvim-treesitter",
+      },
+      {
+        "anuvyklack/pretty-fold.nvim",
+        disable = true, -- crash because of this shit
+        config = function()
+          require("pretty-fold").setup({
+            keep_indentation = false,
+            fill_char = "━",
+            sections = {
+              left = {
+                "━ ",
+                function()
+                  return string.rep("*", vim.v.foldlevel)
+                end,
+                " ━┫",
+                "content",
+                "┣",
+              },
+              right = {
+                "┫ ",
+                "number_of_folded_lines",
+                ": ",
+                "percentage",
+                " ┣━━",
+              },
+            },
+          })
+          require("pretty-fold").ft_setup("cpp", {
+            process_comment_signs = false,
+            comment_signs = {
+              "/**", -- c++ doxygen comments
+            },
+            stop_words = {
+              "%s%*", -- a space and star char
+              "@brief%s*", -- '@brief' and any number of spaces after
+              -- or in sigle pattern:
+              -- '%*%s*@brief%s*', -- * -> any number of spaces -> @brief -> all spaces after
+            },
+          })
+          require("pretty-fold").ft_setup("javascriptreact", {
+            process_comment_signs = false,
+            comment_signs = {
+              "/**", -- c++ doxygen comments
+            },
+            stop_words = {
+              "%s%*", -- a space and star char
+              "@brief%s*", -- '@brief' and any number of spaces after
+              -- or in sigle pattern:
+              -- '%*%s*@brief%s*', -- * -> any number of spaces -> @brief -> all spaces after
+            },
+          })
+          require("pretty-fold").ft_setup("typescript", {
+            process_comment_signs = false,
+            comment_signs = {
+              "/**", -- c++ doxygen comments
+            },
+            stop_words = {
+              "%s%*", -- a space and star char
+              "@brief%s*", -- '@brief' and any number of spaces after
+              -- or in sigle pattern:
+              -- '%*%s*@brief%s*', -- * -> any number of spaces -> @brief -> all spaces after
+            },
+          })
+          require("pretty-fold").ft_setup("typescriptreact", {
+            process_comment_signs = false,
+            comment_signs = {
+              "/**", -- c++ doxygen comments
+            },
+            stop_words = {
+              "%s%*", -- a space and star char
+              "@brief%s*", -- '@brief' and any number of spaces after
+              -- or in sigle pattern:
+              -- '%*%s*@brief%s*', -- * -> any number of spaces -> @brief -> all spaces after
+            },
+          })
+          require("pretty-fold").ft_setup("typescript", {
+            process_comment_signs = false,
+            comment_signs = {
+              "/**", -- c++ doxygen comments
+            },
+            stop_words = {
+              "%s%*", -- a space and star char
+              "@brief%s*", -- '@brief' and any number of spaces after
+              -- or in sigle pattern:
+              -- '%*%s*@brief%s*', -- * -> any number of spaces -> @brief -> all spaces after
+            },
+          })
+          require("pretty-fold.preview").setup({ border = "rounded" })
+        end,
       },
       -- ADD PLUGINS THAT ARE _NECESSARY_ FOR REPRODUCING THE ISSUE
     },
@@ -31,17 +116,21 @@ local function load_plugins()
 end
 _G.load_config = function()
   require("telescope").setup()
-  require("telescope").load_extension("fzf")
   vim.wo.foldmethod = "expr"
   vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-  vim.wo.foldtext =
-    [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
-  vim.wo.fillchars = "fold:\\"
   vim.wo.foldnestmax = 3
   vim.wo.foldminlines = 1
 
+  vim.keymap.set(
+      { "n" },
+      "<space>ff",
+      ":Telescope find_files<CR>",
+      {
+        silent = true,
+      }
+    )
   require("nvim-treesitter.configs").setup({
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {"lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
       enable = true,
     },
