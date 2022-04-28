296bb99cd5725463b3a4e55b5b8bca742ca06165
danielnehrig <daniel.nehrig@dnehrig.com>
2022-04-28 03:48:13 +0200

    nvimtree
    Signed-off-by: danielnehrig <daniel.nehrig@dnehrig.com>

--- lua/config/plugins/nvimTree/init.lua
+++ lua/config/plugins/nvimTree/init.lua
@@ -1,53 +1,7 @@
 local M = {}
 
 function M.init()
-  vim.g.nvim_tree_respect_buf_cwd = 1
-  require("nvim-tree").setup({
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {},
-    update_to_buf_dir = {
-      enable = true,
-      auto_open = true,
-    },
-    auto_close = false,
-    open_on_tab = false,
-    hijack_cursor = false,
-    update_cwd = true,
-    diagnostics = {
-      enable = false,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
-    },
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-      ignore_list = {},
-    },
-    system_open = {
-      cmd = nil,
-      args = {},
-    },
-    view = {
-      width = 30,
-      height = 30,
-      side = "left",
-      auto_resize = false,
-      mappings = {
-        custom_only = false,
-        list = {},
-      },
-    },
-    filters = {
-      dotfiles = false,
-      custom = {},
-    },
-  })
+  require("nvim-tree").setup({})
 end
 
 return M
