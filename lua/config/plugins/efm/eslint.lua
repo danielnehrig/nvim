6cc479017829f320e103089b9848598e795735a0
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-05-09 17:38:49 +0200

    check if its available
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/config/plugins/efm/eslint.lua
+++ lua/config/plugins/efm/eslint.lua
@@ -1,11 +1,21 @@
+local fs = require("config.core.fs")
 local linter = "eslint_d"
+local command = string.format(
+  "%s --no-color --format visualstudio --stdin",
+  fs.executable(linter)
+)
+local command_fmt = string.format(
+  "%s --no-color --fix-to-stdout --stdin",
+  fs.executable(linter)
+)
+
 return {
   prefix = linter,
-  lintCommand = "eslint_d --no-color --format visualstudio --stdin",
+  lintCommand = command,
   lintStdin = true,
   lintFormats = { "<text>(%l,%c): %trror %m", "<text>(%l,%c): %tarning %m" },
   lintIgnoreExitCode = true,
-  formatCommand = "eslint_d --no-color --fix-to-stdout --stdin",
+  formatCommand = command_fmt,
   formatStdin = true,
   rootMarkers = {
     ".eslintrc",
--- lua/config/plugins/efm/luafmt.lua
+++ lua/config/plugins/efm/luafmt.lua
@@ -1,4 +1,9 @@
+local fs = require("config.core.fs")
+
+local cli = "luafmt"
+local command = string.format("%s --stdin", fs.executable(cli))
+
 return {
-  formatCommand = "luafmt --stdin",
+  formatCommand = command,
   formatStdin = true,
 }
