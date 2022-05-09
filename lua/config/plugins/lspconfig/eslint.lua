9de236207377bdfaf2584e9b66da0950f9e6624f
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-05-09 21:00:01 +0200

    set to true
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/config/plugins/lspconfig/eslint.lua
+++ lua/config/plugins/lspconfig/eslint.lua
@@ -7,6 +7,7 @@ lspconfig.eslint.setup({
   capabilities = capabilities,
   flags = { debounce_text_changes = 500 },
   on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = true
     if client.resolved_capabilities.document_formatting then
       local au_lsp = vim.api.nvim_create_augroup("efm_lsp", { clear = true })
       vim.api.nvim_create_autocmd("BufWritePre", {
