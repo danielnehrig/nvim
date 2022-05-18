23463c9285689da6fda4f1d4a40b07c56bb93eb9
Daniel Nehrig <daniel.nehrig@redteclab.com>
2022-05-19 01:02:15 +0200

    add lsp integration
    Signed-off-by: Daniel Nehrig <daniel.nehrig@redteclab.com>

---
+++ lua/config/themes/integrations/lsp.lua
@@ -0,0 +1,19 @@
+local theme = require("config.themes").get_colors("base_16")
+
+return {
+  LspReferenceText = {
+    fg = theme.base0F,
+    bg = theme.base04,
+    bold = true,
+  },
+  LspReferenceRead = {
+    fg = theme.base0F,
+    bg = theme.base04,
+    bold = true,
+  },
+  LspReferenceWrite = {
+    fg = theme.base0F,
+    bg = theme.base04,
+    bold = true,
+  },
+}
