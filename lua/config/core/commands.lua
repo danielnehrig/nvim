db6566cce963c14245df56c9302bd4af8c9cdd83
Daniel Nehrig <daniel.nehrig@redteclab.com>
2022-05-19 00:53:24 +0200

    fix commands requires
    Signed-off-by: Daniel Nehrig <daniel.nehrig@redteclab.com>

--- lua/config/core/commands.lua
+++ lua/config/core/commands.lua
@@ -1,15 +1,15 @@
 local M = {}
 M.init = function()
   vim.api.nvim_create_user_command("PackerSync2", function(_)
-    require("config.plugins.configs.funcs").packer_sync()
+    require("config.plugins.funcs").packer_sync()
   end, {})
 
   vim.api.nvim_create_user_command("Colorscheme", function(tbl)
-    require("config.plugins.configs.funcs").switch_theme(tbl.args)
+    require("config.plugins.funcs").switch_theme(tbl.args)
   end, {
     nargs = 1,
     complete = function(_, _, _)
-      return require("config.plugins.configs.funcs").get_themes()
+      return require("config.plugins.funcs").get_themes()
     end,
   })
 
