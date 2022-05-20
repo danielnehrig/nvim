1cedfe6d5bc73c783b0f96f41269e66c20694d04
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-05-20 22:47:00 +0200

    fix which
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/config/plugins/configs/which/init.lua
+++ lua/config/plugins/configs/which/init.lua
@@ -1,1 +1,13 @@
-require("which-key").setup({})
+local M = {}
+
+M.init = function()
+  local present, which = pcall(require, "which-key")
+
+  if not present then
+    return
+  end
+
+  which.setup({})
+end
+
+return M
