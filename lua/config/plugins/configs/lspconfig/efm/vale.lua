8f1c8ae0a6d3a492ad2a0205528b4b2bd45cf3ca
Daniel Nehrig <daniel.nehrig@redteclab.com>
2022-05-28 04:13:38 +0200

    .vale
    Signed-off-by: Daniel Nehrig <daniel.nehrig@redteclab.com>

--- lua/config/plugins/configs/lspconfig/efm/vale.lua
+++ lua/config/plugins/configs/lspconfig/efm/vale.lua
@@ -1,7 +1,7 @@
 local fs = require("config.core.fs")
 local linter = "vale"
 local command = string.format(
-  "%s --output=%s --",
+  "%s --output=line --",
   fs.executable(linter),
   vim.fn.expand("$HOME/.config/nvim/utils/linter-config/output.tmpl")
 )
@@ -11,7 +11,7 @@ print(command)
 return {
   prefix = linter,
   lintCommand = command,
-  lintFormats = { "%f:%l:%c:%trror:%m", "%f:%l:%c:%tarning:%m" },
+  lintFormats = { "%f:%l:%c:%m" },
   lintIgnoreExitCode = true,
   rootMarkers = {
     ".vale.ini",
