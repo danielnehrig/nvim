bdb57a0cdecb7c26fc64eb603f190a220a96f1c1
nehmee <nehmee@nehmees-MBP.fritz.box>
2023-04-08 02:27:20 +0200

    refactor global
    Signed-off-by: nehmee <nehmee@nehmees-MBP.fritz.box>

--- lua/config/core/global.lua
+++ lua/config/core/global.lua
@@ -4,28 +4,26 @@ local path_sep = package.config:sub(1, 1)
 --- @type string
 local os_name = vim.loop.os_uname().sysname
 
-local global = {}
-global._index = global
-function global:load_variables()
-  self.is_darwin = os_name == "Darwin"
-  self.is_linux = os_name == "Linux"
-  self.is_windows = os_name == "Windows"
-  self.os_name = os_name
-  self.border_style = "rounded"
-  self.sumenko_os = os_name == "Darwin" and "macOS" or "Linux"
-  self.vim_path = vim.fn.stdpath("config")
-  self.path_sep = path_sep
-  self.home = home
-  self.dap_path = vim.fn.stdpath("data") .. path_sep .. "dapinstall"
-  self.lsp_path = vim.fn.stdpath("data") .. path_sep .. "lsp"
-  self.data_path = string.format(
+local M = {
+  is_darwin = os_name == "Darwin",
+  is_linux = os_name == "Linux",
+  is_windows = os_name == "Windows",
+  os_name = os_name,
+  border_style = "rounded",
+  sumenko_os = os_name == "Darwin" and "macOS" or "Linux",
+  vim_path = vim.fn.stdpath("config"),
+  path_sep = path_sep,
+  home = home,
+  dap_path = vim.fn.stdpath("data") .. path_sep .. "dapinstall",
+  lsp_path = vim.fn.stdpath("data") .. path_sep .. "lsp",
+  data_path = string.format(
     "%s" .. path_sep .. "site" .. path_sep,
     vim.fn.stdpath("data")
-  )
-end
+  ),
+}
 
 --- Reload the Config
-function global.reload_all()
+function M.reload_all()
   for k, _ in pairs(package.loaded) do
     if string.match(k, "^config") then
       package.loaded[k] = nil
@@ -36,7 +34,7 @@ function global.reload_all()
   vim.cmd("doautocmd VimEnter")
 end
 
-function global.reload(plugins)
+function M.reload(plugins)
   local status = true
   local function _reload_plugin(plugin)
     local loaded = package.loaded[plugin]
@@ -63,10 +61,4 @@ function global.reload(plugins)
   return status
 end
 
-local global_instance = nil
-if not global_instance then
-  global_instance = global
-  global_instance:load_variables()
-end
-
-return global_instance
+return M
--- lua/config/main.lua
+++ lua/config/main.lua
@@ -10,12 +10,17 @@ local g, opt = vim.g, vim.opt
 -- if not do not apply plugins
 -- slows down VSCode and makes it non usable
 if not g.vscode then
-  package.path = package.path .. ";" .. vim.fn.expand("~/.local/share/nvim/plugin/?.lua")
+  package.path = package.path
+    .. ";"
+    .. vim.fn.expand("~/.local/share/nvim/plugin/?.lua")
   -- setup conf and lua modules
-  require("config.core.global")
   require("config.core.options").load_options()
-  if vim.fn.filereadable(vim.fn.expand("~/.local/share/nvim/plugin/packer_compiled.lua")) == 1 then
-    require('packer_compiled')
+  if
+    vim.fn.filereadable(
+      vim.fn.expand("~/.local/share/nvim/plugin/packer_compiled.lua")
+    ) == 1
+  then
+    require("packer_compiled")
   end
   require("config.core.mappings").mappings()
   require("config.core.autocmd").autocmds()
@@ -27,7 +32,6 @@ if not g.vscode then
 
   opt.shadafile = ""
 else
-
   vim.g.did_load_filetypes = 0
   vim.g.do_filetype_lua = 1
   vim.opt.wildignore:append({
