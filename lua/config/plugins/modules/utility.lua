32008513ae444c7bd37afa06cf74368da0785e1f
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2023-07-22 06:17:09 +0200

    tagging overseer

--- lua/config/plugins/modules/utility.lua
+++ lua/config/plugins/modules/utility.lua
@@ -11,6 +11,26 @@ M.utility = {
       require("compiler").setup(opts)
     end,
   },
+  ["stevearc/overseer.nvim"] = {
+    commit = "3047ede61cc1308069ad1184c0d447ebee92d749", -- Recommended to to avoid breaking changes
+    cmd = { "CompilerOpen", "CompilerToggleResults" },
+    opts = {
+      -- Tasks are disposed 5 minutes after running to free resources.
+      -- If you need to close a task inmediatelly:
+      -- press ENTER in the menu you see after compiling on the task you want to close.
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+        bindings = {
+          ["q"] = function()
+            vim.cmd("OverseerClose")
+          end,
+        },
+      },
+    },
+  },
   ["folke/noice.nvim"] = {
     event = "VimEnter",
     enabled = function()
