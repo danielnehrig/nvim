ee06b7f7adaf4aeca9df4b5ea9898a383482bd3d
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2023-10-17 05:01:14 +0200

    update indent blk
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- lua/config/plugins/configs/indent-blankline/init.lua
+++ lua/config/plugins/configs/indent-blankline/init.lua
@@ -2,37 +2,36 @@
 local M = {}
 
 function M.init()
-  local present, blank = pcall(require, "indent_blankline")
+  local present, blank = pcall(require, "ibl")
   if not present then
     return
   end
   blank.setup({
-    indentLine_enabled = 1,
-    char = "▏",
-    filetype_exclude = {
-      "help",
-      "terminal",
-      "dashboard",
-      "alpha",
-      "lazy",
-      "NvimTree",
-      "packer",
-      "noice",
-      "notify",
-      "lspinfo",
-      "TelescopePrompt",
-      "OverseerForm",
-      "TelescopeResults",
-      "lsp-installer",
-      "",
+    enabled = true,
+    exclude = {
+      buftypes = {
+        "terminal",
+        "dashboard",
+        "notify",
+      },
+      filetypes = {
+        "help",
+        "terminal",
+        "dashboard",
+        "alpha",
+        "lazy",
+        "NvimTree",
+        "packer",
+        "noice",
+        "notify",
+        "lspinfo",
+        "TelescopePrompt",
+        "OverseerForm",
+        "TelescopeResults",
+        "lsp-installer",
+        "",
+      }
     },
-    buftype_exclude = { "terminal", "dashboard", "notify" },
-    show_first_indent_level = true,
-    show_current_context = true,
-    use_treesitter = true,
-    space_char_blankline = " ",
-    show_end_of_line = true,
-    show_trailing_blankline_indent = true,
   })
 end
 
