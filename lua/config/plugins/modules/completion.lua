6726c99e65d8e6dafb22fc7fb1b61f2118872885
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2025-01-02 05:42:07 +0100

    fix: readme gramma

--- lua/config/plugins/modules/completion.lua
+++ lua/config/plugins/modules/completion.lua
@@ -10,6 +10,15 @@ M.completion = {
     enabled = true,
     opts = {
       api_key_cmd = "op read op://Employee/openai/password --no-newline",
+      openai_params = {
+        model = "gpt-4-1106-preview",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 4095,
+        temperature = 0.2,
+        top_p = 0.1,
+        n = 1,
+      },
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
